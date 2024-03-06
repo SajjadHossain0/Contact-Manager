cae385b5811a55731acc7f629854982759086a71
Clock <mdsajjadhossain614@gmail.com>
2024-03-07 01:01:09 +0600

    Contact Manager

--- src/main/java/com/contact/manager/config/MyConfig.java
+++ src/main/java/com/contact/manager/config/MyConfig.java
@@ -1,9 +1,21 @@
 package com.contact.manager.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
-public class MyConfig{
+@EnableWebSecurity
+public class MyConfig implements WebSecurityConfigurer {
 
+    @Override
+    public void init(SecurityBuilder builder) throws Exception {
 
+    }
+
+    @Override
+    public void configure(SecurityBuilder builder) throws Exception {
+
+    }
 }
