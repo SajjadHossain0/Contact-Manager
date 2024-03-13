c3797c12dff7373ec22c6bff14b8da8213e00c26
Clock <mdsajjadhossain614@gmail.com>
2024-03-14 01:46:32 +0600

    Contact Manager

--- src/main/java/com/contact/manager/config/MyConfig.java
+++ src/main/java/com/contact/manager/config/MyConfig.java
@@ -1,21 +1,58 @@
 package com.contact.manager.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.aot.generate.ValueCodeGenerator.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class MyConfig implements WebSecurityConfigurer {
+public class MyConfig{
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz
+                        .anyRequest()
+                        .authenticated())
+                .httpBasic(withDefaults());
 
-    @Override
-    public void init(SecurityBuilder builder) throws Exception {
-
+        return http.build();
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/ignore1", "/ignore2");
     }
 
-    @Override
-    public void configure(SecurityBuilder builder) throws Exception {
+    @Bean
+    public UserDetailsService getUserDetailService(){
 
+        return new UserDetailsServiceImplementation();
     }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(this.getUserDetailService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return daoAuthenticationProvider;
+    }
+
+
 }
